2aec8d2a1c1bb388f8f6770e6f4524583c9608f8
Rakshit Vaja <Rakshit.Vaja@jda.com>
2025-09-19 13:21:27 +0530

    Added ci/cd workflow

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
+
+const ProtectedRoute = () => {
+  const token = useSelector((state: RootState) => state.auth.token);
+
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
